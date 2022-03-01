410aca4b669811662ceb88e5f2caf241fe4a34ae
cho woonghyun <tofan12312@gmail.com>
2022-03-01 16:54:09 +0900

    220301 Spring DIP, OCP 학습

--- Spring/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ Spring/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,8 +1,13 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
+    // 오로지 추상화에만 의존하며, DIP를 의존한다.
+    // 생성자 주입
+    private final MemberRepository memberRepository;
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- Spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Spring/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,14 +2,30 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    // DIP에 위배된다. ServiceImpl이 구현 클래스에 의존하고 있으므로
+    // 좋은 객체 지향 언어는 인터페이스에만 의존해야지, 구현 클래스에는 의존해선 안된다.
+
+    // 따라서 코드를 다음과 같이 변경해 보자.
+    // 철저하게 DIP를 지키고 있다.
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    // 어떤 discountPolicy가 들어올 지, 전혀 예상할 수 없다.
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
