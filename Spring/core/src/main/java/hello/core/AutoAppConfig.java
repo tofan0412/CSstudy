974fc25a5eef7fcb28c0cac6f9d03773b934311b
cho woonghyun <tofan12312@gmail.com>
2022-03-31 18:05:10 +0900

    220331 Spring @ComponentScan 예외처리  학습

--- Spring/core/src/main/java/hello/core/AutoAppConfig.java
+++ Spring/core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan( // 자동으로 Bean을 등록하기 위한 어노테이션
+        basePackages = "hello.core",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // 예외 대상 적용. AppConfig를 등록하지 않기 위해..
 )
 public class AutoAppConfig {
