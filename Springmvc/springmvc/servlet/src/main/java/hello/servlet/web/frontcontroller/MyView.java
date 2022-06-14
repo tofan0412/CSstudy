b5eeae4179d0ce670567e6e1e1519286b22f7932
Admin <tofan@naver.com>
2022-06-14 16:11:44 +0900

    220614 springmvc adapter 학습

--- Springmvc/springmvc/servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ Springmvc/springmvc/servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -19,7 +19,13 @@ public class MyView {
         dispatcher.forward(request, response);
     }
 
-    public void render(Map<String, Object> model, HttpServletRequest req, HttpServletResponse resp) {
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+        modelToRequestAttribute(model, request);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
 
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach((key, value) -> request.setAttribute(key, value));
     }
 }
--- Springmvc/springmvc/servlet/src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
+++ Springmvc/springmvc/servlet/src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -1,14 +1,14 @@
-package hello.servlet.web.frontcontroller.v3;
+package hello.servlet.web.frontcontroller.v4;
 
 import hello.servlet.web.frontcontroller.ModelView;
 import hello.servlet.web.frontcontroller.MyView;
-import hello.servlet.web.frontcontroller.v2.ControllerV2;
-import hello.servlet.web.frontcontroller.v2.controller.MemberFormControllerV2;
-import hello.servlet.web.frontcontroller.v2.controller.MemberListControllerV2;
-import hello.servlet.web.frontcontroller.v2.controller.MemberSaveControllerV2;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -19,15 +19,15 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(name = "frontControllerServletV3", urlPatterns = "/front-controller/v3/*")
-public class FrontControllerServletV3 extends HttpServlet {
+@WebServlet(name = "frontControllerServletV4", urlPatterns = "/front-controller/v4/*")
+public class FrontControllerServletV4 extends HttpServlet {
 
-    private Map<String, ControllerV3> controllerMap = new HashMap<>();
+    private Map<String, ControllerV4> controllerMap = new HashMap<>();
 
-    public FrontControllerServletV3() {
-        controllerMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
-        controllerMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
-        controllerMap.put("/front-controller/v3/members", new MemberListControllerV3());
+    public FrontControllerServletV4() {
+        controllerMap.put("/front-controller/v4/members/new-form", new MemberFormControllerV4());
+        controllerMap.put("/front-controller/v4/members/save", new MemberSaveControllerV4());
+        controllerMap.put("/front-controller/v4/members", new MemberListControllerV4());
     }
 
     @Override
@@ -35,7 +35,7 @@ public class FrontControllerServletV3 extends HttpServlet {
 
         String requestURI = req.getRequestURI();
         // URL의 종류에 따라 호출되는 객체가 달라질것이다.
-        ControllerV3 controller = controllerMap.get(requestURI);
+        ControllerV4 controller = controllerMap.get(requestURI);
 
         if (controller == null) {
             resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
@@ -44,13 +44,13 @@ public class FrontControllerServletV3 extends HttpServlet {
 
         // 이제 paramMap을 넘겨준다.
         Map<String, String> paramMap = createParamMap(req);
-        ModelView mv = controller.process(paramMap);
+        Map<String, Object> model = new HashMap<>(); // 추가
+        String viewName = controller.process(paramMap, model);
 
         // 이제 논리 이름을 물리 이름으로 변경해야 한다. (viewResolver)
-        String viewName = mv.getViewName();
         MyView view = viewResolver(viewName);
 
-        view.render(mv.getModel(), req, resp);
+        view.render(model, req, resp);
     }
 
     private MyView viewResolver(String viewName) {
